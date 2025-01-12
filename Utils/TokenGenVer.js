caf8201049f70adc84fa6bf3891be008e70d0022
Md Tarique Hasan <TariqueMdHasan@gmail.com>
2025-01-13 02:21:09 +0530

    now mogodb url is not exposed

---
+++ Utils/TokenGenVer.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken')
+
+const generateToken = (id) => {
+    try{
+
+    }catch(error){
+        console.log('Error in token generation')
+        throw erro
+    }
+}
